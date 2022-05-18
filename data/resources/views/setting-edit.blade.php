8e5e14334bad6957f2d50a90c5b9f68c6c134d86
hltree <35206336+hltree@users.noreply.github.com>
2022-05-18 14:50:31 +0900

    update: インラインstyleをcssファイルに移行する

--- data/resources/views/setting-edit.blade.php
+++ data/resources/views/setting-edit.blade.php
@@ -1,25 +1,4 @@
 @include('header')
-<style>
-    .alert {
-        position: relative;
-        padding: 0.75rem 1.25rem;
-        margin-bottom: 1rem;
-        border: 1px solid transparent;
-        border-radius: 0.25rem;
-    }
-
-    .alert-danger {
-        color: #721c24;
-        background-color: #f8d7da;
-        border: 1px solid #f5c6cb;
-    }
-
-    .alert-success {
-        color: #155724;
-        background-color: #d4edda;
-        border-color: #c3e6cb;
-    }
-</style>
 <h2>設定</h2>
 <p>休日設定をして保存をクリックしてください</p>
 @if(isset($cEroors))
--- data/resources/views/setting.blade.php
+++ data/resources/views/setting.blade.php
@@ -1,15 +1,4 @@
 @include('header')
-<style>
-    .alert.alert-danger {
-        color: #721c24;
-        background-color: #f8d7da;
-        position: relative;
-        padding: 0.75rem 1.25rem;
-        margin-bottom: 1rem;
-        border: 1px solid #f5c6cb;
-        border-radius: 0.25rem
-    }
-</style>
 <h2>設定</h2>
 <p>このページではカレンダーの休日設定ができます。</p>
 @isset($cEroors)
